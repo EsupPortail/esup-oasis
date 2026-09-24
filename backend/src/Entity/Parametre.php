3b423f5c4b714b732019c2b0a895f46c9e5139f7
Manuel Rossard <95523073+mrossard@users.noreply.github.com>
2026-09-24 10:35:41 +0200

    feat: personnalisation decision (#200)
    Variabilisation des templates et ajout de paramètres dédiés.

--- backend/src/Entity/Parametre.php
+++ backend/src/Entity/Parametre.php
@@ -40,6 +40,12 @@ class Parametre
     public const string APP_TITRE = 'APP_TITRE';
     public const string APP_SERVICE_DENOMINATION = 'APP_SERVICE_DENOMINATION';
     public const string APP_SERVICE_ARTICLE = 'APP_SERVICE_ARTICLE';
+    public const string PRESIDENT_QUALITE = 'PRESIDENT_QUALITE';
+    public const string PRESIDENT_NOM = 'PRESIDENT_NOM';
+    public const string RESPONSABLE_PHASE_NOM = 'RESPONSABLE_PHASE_NOM';
+    public const string RESPONSABLE_PHASE_QUALITE = 'RESPONSABLE_PHASE_QUALITE';
+    public const string LIEU_COURRIER = 'LIEU_COURRIER';
+    public const string ADRESSE_POSTALE = 'ADRESSE_POSTALE';
 
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'SEQUENCE')]
